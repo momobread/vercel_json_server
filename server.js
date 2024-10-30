399b837c7751736e55d59813bfb8513b0e709f69
박시은 <si990819@gmail.com>
2024-10-30 16:30:42 +0900

    d

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('post.json');
-
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
