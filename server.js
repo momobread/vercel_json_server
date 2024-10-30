5f0a0e997496ca1ba698e47e35a2f6c805750076
박시은 <si990819@gmail.com>
2024-10-30 15:59:19 +0900

    user 테스트

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('post.json');
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
